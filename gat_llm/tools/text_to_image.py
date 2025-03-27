541b99472504511c054cc43478fee7c11d67e5a2
DOUGLAS COIMBRA DE ANDRADE <douglas.coimbra@sc.senai.br>
2025-03-27 17:30:46 -0300

    Add Bedrock stablediffusion image generator

--- gat_llm/tools/text_to_image.py
+++ gat_llm/tools/text_to_image.py
@@ -1,5 +1,6 @@
 import os
 import json
+import boto3
 import base64
 
 import numpy as np
@@ -9,9 +10,51 @@ rng = np.random.default_rng()
 
 
 class ToolTextToImage:
+    def _gen_img_bedrock(self, input_text, target_file, default_region="us-west-2"):
+        if self.bedrock_client is None:
+            self.bedrock_client = boto3.client("bedrock-runtime", region_name=default_region)
+        model_id = "stability.stable-image-ultra-v1:0"
+
+        # seed = int(rng.integers(low=0, high=4294967295))
+        native_request = {
+            "prompt": input_text,
+        }
+        # Convert the native request to JSON.
+        request = json.dumps(native_request)
+
+        # Invoke the model with the request.
+        response = self.bedrock_client.invoke_model(modelId=model_id, body=request)
+
+        output_body = json.loads(response["body"].read().decode("utf-8"))
+        base64_output_image = output_body["images"][0]
+
+        # Extract the image data.
+        image_data = base64.b64decode(base64_output_image)
+        with open(target_file, "wb") as file:
+            file.write(image_data)
+        return input_text
+
+    def _gen_img_openai(self, input_text, target_file):
+        if self.openai_client is None:
+            self.openai_client = OpenAI()
+        response = self.openai_client.images.generate(
+            model="dall-e-3",
+            prompt=input_text,
+            n=1,
+            size="1024x1024",
+            response_format="b64_json",
+        )
+        parsed_ans = json.loads(response.json())
+        revised_prompt = parsed_ans["data"][0]["revised_prompt"]
+        img_content = base64.b64decode(parsed_ans["data"][0]["b64_json"])
+        with open(target_file, "wb") as f:
+            f.write(img_content)
+        return revised_prompt
+
     def __init__(self):
         self.name = "text_to_image"
 
+        self.valid_engines = ["openai-dalle3", "bedrock-stablediffusion"]
         self.tool_description = {
             "name": self.name,
             "description": """Uses a text-to-image tool to convert the provided text into an image. Uses neural methods such as diffusion.
@@ -25,39 +68,45 @@ Raises ValueError: if not able to generate the image.""",
                         "type": "string",
                         "description": "Text that will be converted to image.",
                     },
+                    "engine": {
+                        "type": "string",
+                        "description": f"Engine that will be used to generate the image. Valid values are: {self.valid_engines}. Default value is {self.valid_engines[0]}",
+                    },
                 },
                 "required": ["input_text"],
             },
         }
         self.openai_client = None
+        self.bedrock_client = None
 
     def __call__(
         self,
         input_text,
+        engine=None,
         **kwargs,
     ):
         os.makedirs("media", exist_ok=True)
-        if self.openai_client is None:
-            self.openai_client = OpenAI()
         if len(kwargs) > 0:
             return f"Error: Unexpected parameter(s): {','.join([x for x in kwargs])}"
 
         rng_num = rng.integers(low=0, high=900000)
         target_file = f"media/gen_image_{rng_num}.png"
 
+        if engine is None:
+            if os.getenv("OPENAI_API_KEY"):
+                engine = "openai-dalle3"
+            else:
+                engine = "bedrock-stablediffusion"
+        engine = engine.lower()
+
+        if engine not in self.valid_engines:
+            return f"Invalid text-to-image engine: {engine}. Valid engines are: {self.valid_engines}"
+
         try:
-            response = self.openai_client.images.generate(
-                model="dall-e-3",
-                prompt=input_text,
-                n=1,
-                size="1024x1024",
-                response_format="b64_json",
-            )
-            parsed_ans = json.loads(response.json())
-            revised_prompt = parsed_ans["data"][0]["revised_prompt"]
-            img_content = base64.b64decode(parsed_ans["data"][0]["b64_json"])
-            with open(target_file, "wb") as f:
-                f.write(img_content)
+            if engine == "openai-dalle3":
+                revised_prompt = self._gen_img_openai(input_text, target_file)
+            elif engine == "bedrock-stablediffusion":
+                revised_prompt = self._gen_img_bedrock(input_text, target_file)
         except Exception as e:
             return f"Image was NOT generated.\nError description: {str(e)}"
 
@@ -66,7 +115,7 @@ Raises ValueError: if not able to generate the image.""",
 
         ans = ["<image>"]
         ans.append(
-            f"<revised_prompt>{revised_prompt}</revised_prompt><path_to_image>{target_file}</path_to_image>"
+            f"<revised_prompt>{revised_prompt}</revised_prompt><used_engine>{engine}</used_engine><path_to_image>{target_file}</path_to_image>"
         )
         ans.append("</image>")
         return "\n".join(ans)
